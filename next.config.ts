08c066217f27db074124639927169b12d4041581
Kunal Anarse <83918721+Kunal-027@users.noreply.github.com>
2026-03-15 15:04:05 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,13 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
   /** Use this directory as workspace root so Next doesn’t pick a parent lockfile. */
   outputFileTracingRoot: path.join(__dirname),
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
